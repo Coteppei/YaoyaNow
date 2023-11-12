a7cad4c61e353c6a3d85bf1df15438fed5c33097
Teppei Koshiji <Coolandhotman@outlook.jp>
2023-11-13 00:04:13 +0900

    ** アカウント新規登録の異常入力対策 ** - XSS攻撃対策ができていなかったため、特殊文字をHTMLエンティティに変換 - ユーザー名が半角、全角スペースのみの場合でも登録できていたため、登録不可になるように対応 - メールアドレス入力がメールアドレス形式出なかった場合も登録不可になるように対応。 - F12でフロント側コードをいじられると各入力欄が未入力で登録できたため、一項目でも未入力の場合、エラー画面に飛ぶように対応

--- back_end/account_register.php
+++ back_end/account_register.php
@@ -1,9 +1,11 @@
 <?php
 require_once("common.php");
 session_start();
+// 空文字対策
+$error_letter = "^(\s|　)+$";
 
 // 正常に入力データが届いていない場合、エラー画面に遷移
-if (!isset($_POST['username']) || !isset($_POST['password']) || !isset($_POST['email'])) {
+if (!isset($_POST['username']) || empty($_POST['username']) || !isset($_POST['password']) || empty($_POST['password']) || !isset($_POST['email']) || empty($_POST['email'])) {
   header('Location: ../error.php');
   exit;
 }
@@ -11,9 +13,18 @@ if (!isset($_POST['username']) || !isset($_POST['password']) || !isset($_POST['e
 // 登録ボタンを押すと作動
 try {
   if (isset($_POST['registerButton'])) {
-    // 入力情報の定義
-    $user_name = $_POST['username'];
-    $email = $_POST['email'];
+    if (mb_ereg_match("^(\s|　)+$", $_POST['username'])) {  // ユーザー名が半角、全角スペースのみでの入力があった場合、登録不可と判定。 
+      $_SESSION['action_message'] = '空文字では登録できません。';
+      header('Location: ../signUp.php');
+      exit;
+    } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {  // 形式がメールアドレスでなかった場合、登録不可と判定。
+      $_SESSION['action_message'] = 'メールアドレス形式で入力してください。';
+      header('Location: ../signUp.php');
+    } else {  // 正常な入力データの場合、各入力情報を定義する。
+      $user_name = htmlspecialchars($_POST['username'],ENT_QUOTES, 'UTF-8');
+      $email = htmlspecialchars($_POST['email'],ENT_QUOTES, 'UTF-8');
+    }
+    
     // ユーザー名とメールアドレスが両方とも既に登録されているか確認
     $check_stmt = $pdo->prepare("SELECT ID FROM customers WHERE user_name = ? AND email = ?");
     $check_stmt->bindValue(1, $user_name, PDO::PARAM_STR);
@@ -50,5 +61,5 @@ try {
     }
   }
 } catch (PDOException $e) {
-  echo "データベースエラー: " . $e->getMessage();
+  echo "データベースエラー: " . $e->getMessage();  
 }
