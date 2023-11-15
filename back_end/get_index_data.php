b7d0e72f5aece8fac2d87aac321dca0a128e59b8
Teppei Koshiji <Coolandhotman@outlook.jp>
2023-11-15 12:22:30 +0900

    ** ページング対応 ** - 商品状態が多い場合、別ページに続きの内容が表示されるように対応。

--- back_end/get_index_data.php
+++ back_end/get_index_data.php
@@ -1,8 +1,39 @@
 <?php
 require_once("common.php");
 session_start();
-// 野菜テーブルのセレクト
-$query = "SELECT * FROM vegetable";
-$stmt = $pdo->prepare($query);
-$stmt->execute();
-$vegetablesdata = $stmt->fetchAll();
+
+// 1ページに表示する画像の数を定義
+define('MAX', 8);
+
+// 野菜テーブルの画像の総数をカウント
+$queryTotal = "SELECT COUNT(*) AS total FROM vegetable";
+$stmtTotal = $pdo->prepare($queryTotal);
+$stmtTotal->execute();
+$total = $stmtTotal->fetchColumn();
+
+// 総ページ数を算出
+$total_page = ceil($total / MAX);
+
+// 現在ページ情報の取得
+if (isset($_GET['page'])) {
+  // ページが指定されているとき
+  $currentPage = $_GET['page'];
+} else {  
+  // ページが指定されていない場合は1ページ目を表示 
+  $currentPage = 1;
+}
+
+// 現在のページ番号を取得する
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+
+// ページ数が1未満の場合は1に、ページ数が総ページ数を超える場合は総ページ数に制限する
+$page = max(1, min($page, $total_page));
+
+// 表示する画像のデータを取得する
+$offset = ($page - 1) * MAX;
+$queryImages = "SELECT * FROM vegetable LIMIT :offset, :max";
+$stmtImages = $pdo->prepare($queryImages);
+$stmtImages->bindValue(':offset', $offset, PDO::PARAM_INT);
+$stmtImages->bindValue(':max', MAX, PDO::PARAM_INT);
+$stmtImages->execute();
+$vegetablesdata = $stmtImages->fetchAll();
--- index.php
+++ index.php
@@ -31,6 +31,26 @@ require_once("./back_end/get_index_data.php");
                 </li>
             <?php endforeach; ?>
         </ul>
+        <!-- ページングの表示 -->
+        <div class="all-pagination">
+            <?php if ($currentPage > 1): ?>
+                <a href="?page=<?php echo $currentPage - 1; ?>" class="PC-display mr-4">≪前のページへ</a>
+                <a href="?page=<?php echo $currentPage - 1; ?>" class="SP-display mr-4">≪前へ</a>
+            <?php endif; ?>
+            <div class="pagination">
+                <?php for ($page = 1; $page <= $total_page; $page++): ?>
+                    <?php if ($page == $currentPage): ?>
+                        <p><?php echo $page; ?></p>
+                    <?php else: ?>
+                        <a href="?page=<?php echo $page; ?>"><?php echo $page; ?></a>
+                    <?php endif; ?>
+                <?php endfor; ?>
+            </div>
+            <?php if ($currentPage < $total_page): ?>
+                <a href="?page=<?php echo $currentPage + 1; ?>" class="PC-display ml-4">次のページへ≫</a>
+                <a href="?page=<?php echo $currentPage + 1; ?>" class="SP-display ml-4">次へ≫</a>
+            <?php endif; ?>
+        </div>
     </div>
     <?php
     include('./front_end_common/footer.php');
